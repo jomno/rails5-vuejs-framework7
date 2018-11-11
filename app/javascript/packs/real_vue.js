9579246a8102361b0a33339bc1db56777c9dbf4f
jomno <jake8955@naver.com>
2018-11-12 00:49:15 +0900

    real_vue와 test_vue와 분리함

--- app/javascript/packs/real_vue.js
+++ app/javascript/packs/real_vue.js
@@ -23,13 +23,14 @@ import Framework7 from 'framework7/framework7.esm.bundle.js'
 import Framework7Vue from 'framework7-vue/framework7-vue.esm.bundle.js'
 
 Framework7.use(Framework7Vue);
-import App from '../app.vue';
+
+import App from '../app.vue'
 
 document.addEventListener('DOMContentLoaded', () => {
-  new Vue({
-    el: '#app',
-    render: (h) => h(App)
-  });
+  const app = new Vue({
+                el: '#app',
+                render: (h) => h(App)
+              });
 })
 
 
